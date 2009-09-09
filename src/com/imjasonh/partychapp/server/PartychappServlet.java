e8859bce9e5d8c44939bb9e33bab90b1a0af974a
Jason <imjasonh@gmail.com>
2009-09-09 16:53:02 +0000

    Remove /worker handler, it was eating up quota?  Clean up PartychappServlet

--- src/com/imjasonh/partychapp/server/PartychappServlet.java
+++ src/com/imjasonh/partychapp/server/PartychappServlet.java
@@ -13,7 +13,6 @@ import com.google.appengine.api.xmpp.Message;
 import com.google.appengine.api.xmpp.MessageBuilder;
 import com.google.appengine.api.xmpp.XMPPService;
 import com.google.appengine.api.xmpp.XMPPServiceFactory;
-import com.google.appengine.repackaged.com.google.common.base.Preconditions;
 import com.imjasonh.partychapp.Channel;
 
 @SuppressWarnings("serial")
@@ -21,85 +20,126 @@ public class PartychappServlet extends HttpServlet {
 
   private static final Logger LOG = Logger.getLogger(PartychappServlet.class.getName());
 
-  private static final XMPPService XMPP = XMPPServiceFactory.getXMPPService();
+  private XMPPService XMPP;
+
+  private JID userJID;
+
+  private JID serverJID;
+
+  private String alias;
+
+  private Channel channel;
+
+  private String channelName;
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     super.doPost(req, resp);
 
+    XMPP = XMPPServiceFactory.getXMPPService();
+
     Message message = XMPP.parseMessage(req);
 
-    JID userJID = message.getFromJid();
-    String alias = userJID.getId().split("@")[0]; // TODO don't automatically assume alias from JID
+    userJID = message.getFromJid();
+    alias = userJID.getId().split("@")[0]; // TODO don't automatically assume alias from JID
 
-    JID serverJID = message.getRecipientJids()[0]; // should only be "to" one jid, right?
-    String channelName = serverJID.getId().split("@")[0];
+    serverJID = message.getRecipientJids()[0]; // should only be "to" one jid, right?
+    channelName = serverJID.getId().split("@")[0];
 
     String body = message.getBody().trim();
 
     if (channelName.equals("echo")) {
-      // if the user is talking to echo@, just echo back
-      sendMessage("ECHO: " + body, serverJID, userJID);
+      handleEcho(body);
       return;
     }
 
-    Channel channel = Channel.getByName(channelName);
+    channel = Channel.getByName(channelName);
     if (channel == null) {
       // channel doesn't exist yet
-      channel = new Channel(channelName);
-      channel.addMember(userJID);
-      channel.put();
-      String msg = "The channel '" + channelName + "' has been created, " +
-          "and you have joined with the alias '" + alias + "'";
-      sendMessage(msg, serverJID, userJID);
-      return;
+      channel = handleCreateChannel();
     }
-
-    Preconditions.checkNotNull(channel);
     if (!channel.isMember(userJID)) {
       // user isn't in room yet
-      channel.addMember(userJID);
-      channel.put();
-      String msg = "You have joined '" + channelName + "' with the alias '" + alias + "'";
-      sendMessage(msg, serverJID, userJID);
+      handleJoinChannel();
     }
 
-    if (body.equals("/leave")) {
-      channel.removeMember(userJID);
-      channel.put();
-      sendMessage("You have left '" + channelName + "'", serverJID, userJID);
+    // channel exists, user is in it
 
-      if (!channel.isEmpty()) {
-        // tell everyone else (if they exist)
-        String msg = alias + " has left the room (" + userJID.getId() + ")";
-        sendMessage(msg, serverJID, channel.getAllMemberJIDsArray());
-      }
+    if (body.equals("/leave")) {
+      handleLeave();
     } else if (body.equals("/list")) {
-      StringBuilder sb = new StringBuilder()
-          .append("List of members of '" + channelName + "':").append('\n');
-      for (JID member : channel.getAllMemberJIDsArray()) {
-        sb.append("* ").append(member.getId()).append('\n');
-      }
-      sendMessage(sb.toString(), serverJID, userJID);
+      handleList();
     } else if (body.equals("/commands") || body.equals("/help")) {
-      String msg = new StringBuilder()
-          .append("List of available commands:\n")
-          .append("* /list prints a list of all members subscribed to this channel\n")
-          .append("* /leave unsubscribes you from this channel\n")
-          .append("* Send a message to echo@... to hear yourself talk")
-          .toString();
-      sendMessage(msg, serverJID, userJID);
+      handleHelp();
     } else {
-      // user is in room now, send message to everyone else (if they exist)
-      JID[] recipients = channel.getAllMemberJIDsArrayExcept(userJID);
-      if (recipients.length > 0) {
-        sendMessage("['" + alias + "'] " + body, serverJID, recipients);
-      }
+      // just a normal message to broadcast
+      handleMessage(body);
     }
   }
 
-  private static void sendMessage(String body, JID fromJID, JID... toJID) {
+  private void handleMessage(String body) {
+    JID[] recipients = channel.getAllMemberJIDsArrayExcept(userJID);
+    if (recipients.length > 0) {
+      sendMessage("['" + alias + "'] " + body, serverJID, recipients);
+    }
+  }
+
+  private void handleHelp() {
+    String msg = new StringBuilder()
+        .append("List of available commands:\n")
+        .append("* /list prints a list of all members subscribed to this channel\n")
+        .append("* /leave unsubscribes you from this channel\n")
+        .append("* Send a message to echo@... to hear yourself talk")
+        .toString();
+    sendMessage(msg, serverJID, userJID);
+  }
+
+  private void handleList() {
+    StringBuilder sb = new StringBuilder()
+        .append("List of members of '" + channelName + "':").append('\n');
+    for (JID member : channel.getAllMemberJIDsArray()) {
+      sb.append("* ").append(member.getId()).append('\n');
+    }
+    sendMessage(sb.toString(), serverJID, userJID);
+  }
+
+  private void handleLeave() {
+    channel.removeMember(userJID);
+    channel.put();
+    sendMessage("You have left '" + channelName + "'", serverJID, userJID);
+
+    if (!channel.isEmpty()) {
+      // tell everyone else (if they exist)
+      String msg = alias + " has left the room (" + userJID.getId() + ")";
+      sendMessage(msg, serverJID, channel.getAllMemberJIDsArray());
+    }
+  }
+
+  private void handleJoinChannel() {
+    channel.addMember(userJID);
+    channel.put();
+    String msg = "You have joined '" + channelName + "' with the alias '" + alias + "'";
+    sendMessage(msg, serverJID, userJID);
+  }
+
+  private Channel handleCreateChannel() {
+    Channel channel;
+    channel = new Channel(channelName);
+    channel.addMember(userJID);
+    channel.put();
+    String msg = "The channel '" + channelName + "' has been created, " +
+        "and you have joined with the alias '" + alias + "'";
+    sendMessage(msg, serverJID, userJID);
+    return channel;
+  }
+
+  private void handleEcho(String body) {
+    // if the user is talking to echo@, just echo back
+    sendMessage("ECHO: " + body, serverJID, userJID);
+  }
+
+  private void sendMessage(String body, JID fromJID, JID... toJID) {
     XMPP.sendMessage(new MessageBuilder()
         .withRecipientJids(toJID)
         .withFromJid(fromJID)
