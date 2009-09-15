1b50b1d5934ebeaa6f4ef57f065736e7f57b3fd1
Jason <imjasonh@gmail.com>
2009-09-15 16:50:16 +0000

    Add bug-filing URL to /help

--- src/com/imjasonh/partychapp/server/command/HelpHandler.java
+++ src/com/imjasonh/partychapp/server/command/HelpHandler.java
@@ -4,7 +4,7 @@ import com.imjasonh.partychapp.Message;
 import com.imjasonh.partychapp.server.SendUtil;
 
 public class HelpHandler extends SlashCommand {
-  
+
   public HelpHandler() {
     super("help", "commands");
   }
@@ -12,19 +12,20 @@ public class HelpHandler extends SlashCommand {
   @Override
   public void doCommand(Message msg, String argument) {
     // TODO: Reject or act on non-null argument
-    
+
     StringBuilder sb = new StringBuilder().append("List of commands:").append('\n');
     for (Command command : Command.values()) {
       sb.append("* ")
           .append(command.commandHandler.documentation())
           .append('\n');
     }
-    sb.append("* Message echo@ to hear yourself talk").append('\n');
+    sb.append("* Message echo@ to hear yourself talk").append('\n')
+        .append("* Found a bug? Let us know: http://code.google.com/p/partychapp/issues/entry");
 
     SendUtil.sendDirect(sb.toString(), msg.userJID, msg.serverJID);
   }
-  
+
   public String documentation() {
-  	return "/help - shows this";
+    return "/help - shows this";
   }
 }
