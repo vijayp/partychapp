04792f9fc300b8db110e4c826e3a2e6e91de5617
David Blackman <whizziwig@gmail.com>
2009-12-12 04:57:26 +0000

    redirect to login link

--- src/com/imjasonh/partychapp/server/WebChannelServlet.java
+++ src/com/imjasonh/partychapp/server/WebChannelServlet.java
@@ -1,37 +1,34 @@
 package com.imjasonh.partychapp.server;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Logger;
 
-import javax.jdo.PersistenceManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.api.xmpp.JID;
 import com.imjasonh.partychapp.Channel;
 import com.imjasonh.partychapp.Datastore;
-import com.imjasonh.partychapp.ppb.Target;
 
 public class WebChannelServlet  extends HttpServlet {
 	@SuppressWarnings("unused")
 	private static final Logger LOG = Logger.getLogger(WebServlet.class.getName());
 
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 		if (user == null) {
-			// TODO(blackmad): redirect to a login page
-			resp.getWriter().write("Sorry, not logged in.");    
+			resp.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+			resp.setHeader("Location", 
+					userService.createLoginURL(req.getRequestURL().toString()));  
 			return;
 		}
 
