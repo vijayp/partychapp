4d442af9037003b091c5c3dbcc098c2966ca9348
Mihai <mihai.parparita@gmail.com>
2010-04-11 02:51:03 +0000

    Always call datastore.endRequest in ChannelServlet (and fix formatting).

--- src/com/imjasonh/partychapp/server/web/ChannelServlet.java
+++ src/com/imjasonh/partychapp/server/web/ChannelServlet.java
@@ -14,36 +14,40 @@ import com.google.appengine.api.users.UserServiceFactory;
 import com.imjasonh.partychapp.Channel;
 import com.imjasonh.partychapp.Datastore;
 
-public class ChannelServlet  extends HttpServlet {
-	public static final long serialVersionUID = 985749740983755L;
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException {
-		UserService userService = UserServiceFactory.getUserService();
-		User user = userService.getCurrentUser();
-		if (user == null) {
-			resp.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-			resp.setHeader("Location", 
-					userService.createLoginURL(req.getRequestURL().toString()));  
-			return;
-		}
+public class ChannelServlet extends HttpServlet {
+  public static final long serialVersionUID = 985749740983755L;
 
-		String[] paths = req.getRequestURI().split("/");
-		String channelName = paths[paths.length - 1];
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+    if (user == null) {
+      resp.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+      resp.setHeader("Location", userService.createLoginURL(req.getRequestURL()
+          .toString()));
+      return;
+    }
 
-		Datastore datastore = Datastore.instance();
-		datastore.startRequest();
+    String[] paths = req.getRequestURI().split("/");
+    String channelName = paths[paths.length - 1];
 
-		Channel channel = datastore.getChannelIfUserPresent(channelName, user.getEmail());
-		if (channel != null) {
-			RequestDispatcher disp;
-			disp = getServletContext().getRequestDispatcher("/channel.jsp");
-			req.setAttribute("channel", channel);
-			disp.forward(req, resp);  
-		} else {
-			resp.getWriter().write("Access denied");  
-			return;
-		}
-	}
+    Datastore datastore = Datastore.instance();
+    datastore.startRequest();
+    try {
+      Channel channel =
+          datastore.getChannelIfUserPresent(channelName, user.getEmail());
+      if (channel != null) {
+        RequestDispatcher disp;
+        disp = getServletContext().getRequestDispatcher("/channel.jsp");
+        req.setAttribute("channel", channel);
+        disp.forward(req, resp);
+      } else {
+        resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+        resp.getWriter().write("Access denied");
+      }
+    } finally {
+      datastore.endRequest();
+    }
+  }
 }
