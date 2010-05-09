58b1d0fe993efde78a2ca010282f41a96b621fd8
Mihai <mihai.parparita@gmail.com>
2010-05-09 21:07:55 +0000

    More Message cleanup.

--- src/com/imjasonh/partychapp/Message.java
+++ src/com/imjasonh/partychapp/Message.java
@@ -1,53 +1,37 @@
 package com.imjasonh.partychapp;
 
 import com.google.appengine.api.xmpp.JID;
+import com.google.common.base.Preconditions;
 
 import com.imjasonh.partychapp.testing.FakeDatastore;
 
 public class Message {
   public enum MessageType { EMAIL, XMPP, SMS }
   
-  public static Message createForTests(String content) {
-    return createForTests(content, MessageType.XMPP);
-  }
-  public static Message createForTests(
-          String content, MessageType messageType) {
-    JID userJID = new JID("neil@gmail.com");
-    Channel c = FakeDatastore.fakeChannel();
-    
-    return new Builder()
-      .setContent(content)
-      .setUserJID(userJID)
-      .setServerJID(new JID("pancake@partychapp.appspotchat.com"))
-      .setMember(c.getMemberByJID(userJID))
-      .setChannel(c)
-      .setMessageType(messageType)
-      .build();
-  }
-
   private Message(Builder builder) {
-    this.content = builder.content;
-    this.userJID = builder.userJID;
-    this.serverJID = builder.serverJID;
+    this.content = Preconditions.checkNotNull(builder.content);
+    this.userJID = Preconditions.checkNotNull(builder.userJID);
+    this.serverJID = Preconditions.checkNotNull(builder.serverJID);
+    this.messageType = Preconditions.checkNotNull(builder.messageType);
+    
     this.member = builder.member;
     this.channel = builder.channel;
-    this.messageType = builder.messageType;
   }
   
   public final String content;
   public final JID userJID;
   public final JID serverJID;
+  public final MessageType messageType;
   public Member member;
   public Channel channel;
-  public final MessageType messageType;
   
   public static class Builder {
     private String content;
     private JID userJID;
     private JID serverJID;
+    private MessageType messageType;
     private Member member;
     private Channel channel;
-    private MessageType messageType;
 
     /**
      * Creates a new Builder that is pre-populated with all of the fields from
@@ -76,6 +60,11 @@ public class Message {
       this.serverJID = serverJID;
       return this;
     }
+    
+    public Builder setMessageType(MessageType messageType) {
+      this.messageType = messageType;
+      return this;
+    }    
 
     public Builder setMember(Member member) {
       this.member = member;
@@ -84,11 +73,6 @@ public class Message {
 
     public Builder setChannel(Channel channel) {
       this.channel = channel;
-      return this;
-    }
-
-    public Builder setMessageType(MessageType messageType) {
-      this.messageType = messageType;
       return this;
     }
 
@@ -105,4 +89,23 @@ public class Message {
         ", channel = " + channel + 
         ", type = " + messageType;
   }
+  
+  public static Message createForTests(String content) {
+    return createForTests(content, MessageType.XMPP);
+  }
+  
+  public static Message createForTests(
+          String content, MessageType messageType) {
+    JID userJID = new JID("neil@gmail.com");
+    Channel c = FakeDatastore.fakeChannel();
+    
+    return new Builder()
+      .setContent(content)
+      .setUserJID(userJID)
+      .setServerJID(new JID("pancake@partychapp.appspotchat.com"))
+      .setMessageType(messageType)
+      .setMember(c.getMemberByJID(userJID))
+      .setChannel(c)
+      .build();
+  }  
 }
