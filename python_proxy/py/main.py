c9c8324f9f05188b05ee0a33dcd8a0082601fa08
Vijay Pandurangan <vijayp@vijayp.ca>
2011-11-09 21:31:57 -0500

    testing ...

--- python_proxy/py/main.py
+++ python_proxy/py/main.py
@@ -6,16 +6,16 @@ from HTTPFrontend import HTTPFrontend
 from SimpleComponent import SimpleComponent
 
 # Uncomment the following line to turn on debugging
-logging.basicConfig(level=logging.INFO, format='%(levelname)-8s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
 
 def main() :
-	backend = {}
 	component = SimpleComponent(
 		jid = "im.partych.at", password = "secret",
-		server = "127.0.0.1", port = 5275, backend = backend)
+		server = "127.0.0.1", port = 5275, backend = None)
 	component.start()
-	httpFrontend = HTTPFrontend(8234, None)
+	httpFrontend = HTTPFrontend(8081, None)
 	httpFrontend.start()
 
 if __name__ == '__main__' :
 	main()
+
